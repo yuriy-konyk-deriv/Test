5483f056a1e91a3187c43afe4a8bc357b64e5aa7
Yuriy Konyk <yuriy.konyk@deriv.com>
2021-11-23 03:48:22 +0200

    Almost done
    UDP + TCP

---
+++ Client/TCP_Client.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#define MaxBuffer 1500
+#define Port 8080
+
+#define Msg "Hey stupid server can u hear me?"
+#define Server_IP "127.0.0.1"
+#define Host_IP "127.0.0.1"
+
+int main() {
+    //printf("Hey");
+    int socket_fd,connect_fd; // socket file descriptor
+    char buffer[MaxBuffer]; // buffer in which client messages will be stored
+    char filename[100];
+    struct sockaddr_in server_address, income_address, client_address;
+
+    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        printf("Error while creating socket\n");
+        return -1;
+    }
+
+    // initialize sockaddr_in with 0
+    memset(&server_address, 0, sizeof(server_address));
+    memset(&income_address, 0, sizeof(income_address));
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr(Server_IP);
+    server_address.sin_port = htons(Port);
+
+    // the current client
+    client_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr(Host_IP);
+    client_address.sin_port = htons(Port);
+
+
+    socklen_t server_size = sizeof(server_address);
+
+    connect_fd = connect(socket_fd, (struct sockaddr*)&server_address, server_size);
+    while (connect_fd != 0) {
+        printf("Server accept failed... press any key to try again\n");
+        getchar();
+        connect_fd = connect(socket_fd, (struct sockaddr*)&server_address, server_size);
+    
+    }
+    printf("Server accepted the client...\n");
+
+    
+    while (printf("Enter file name > ")) {
+        scanf("%s", buffer);
+        memcpy(filename, buffer, strlen(buffer) + 1);
+        printf("Sending \"%s\" to Server\n", buffer);
+        write(socket_fd, buffer, sizeof(buffer));
+        if (strcmp(buffer, "shutdown") == 0) {
+            close(connect_fd);
+            close(socket_fd);
+            printf("Closing client\n");
+            return 0;
+        }
+
+        // wait to see if the file exist on server
+        memset(&buffer, 0, sizeof(buffer));
+        printf("Waiting response from server\n");
+        read(socket_fd, (char*)buffer, sizeof(buffer));
+
+        // response actions
+        printf("Recieved %s from server\n", buffer);
+        if (strcmp(buffer, "miss") == 0) {
+            printf("Caught response, file miss\n");
+            memset(&buffer, 0, sizeof(buffer));
+            int file_fd = open(filename, O_RDONLY);
+            printf("Preparing to send %s file to server\n", filename);
+            if (file_fd == -1) {
+                printf("There is no such file in your directory\n");
+                memset(&buffer, 0, sizeof(buffer));
+                continue;
+            }
+
+            int i = 1;
+            while (read(file_fd, &buffer, MaxBuffer) != 0) {
+                printf("Sending %d segment\n", i);
+                write(socket_fd, buffer, sizeof(buffer));
+                memset(&buffer, 0, sizeof(buffer));
+                //usleep(10);
+                i++;
+            }
+            //printf("Sending %d segment\n", i);
+            //write(socket_fd, buffer, sizeof(buffer));
+            write(socket_fd, "LoLThatzAll", sizeof("LoLThatzAll"));
+            close(file_fd);
+            
+        }
+        else if (strcmp(buffer, "hit") == 0) {
+            printf("Caught response, file exist\n");
+        }
+        memset(&buffer, 0, sizeof(buffer));
+    }
+}
+
+/*
+
+    First check if file exist then send request
+*/
---
+++ TCP_Server.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#define MaxBuffer 1500
+#define Port 8080
+
+#define Server_IP "127.0.0.1"
+#define Host_IP "127.0.0.1"
+
+int main() {
+    int socket_fd, connect_fd; // socket file descriptor
+    char buffer[MaxBuffer]; // buffer in which client messages will be stored
+    char* message = "Howdy, I am the stupid server!"; // Server message
+    struct sockaddr_in server_address, client_address;
+
+    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        printf("Error while creating socket\n");
+        return -1;
+    }
+
+    // initialize sockaddr_in with 0
+    memset(&server_address, 0, sizeof(server_address));
+    memset(&client_address, 0, sizeof(client_address));
+
+    char client_address_buffer[INET_ADDRSTRLEN];
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr(Server_IP);
+    server_address.sin_port = htons(Port);
+
+    // bind socket with server_address
+    if (bind(socket_fd, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
+        printf("Error while binding socket\n");
+        inet_ntop(AF_INET, &server_address.sin_addr, client_address_buffer, sizeof(client_address_buffer));
+        printf("Address is %s\n", client_address_buffer);
+        close(socket_fd);
+        close(connect_fd);
+        return -1;
+    }
+
+    if ((listen(socket_fd, 1)) != 0) {
+        printf("Listen failed...\n");
+        return -1;
+    }
+    else
+        printf("Waitng for connection..\n");
+
+
+    // Accept the data packet from client and verification
+    socklen_t client_length = sizeof(client_address);
+    connect_fd = accept(socket_fd, (struct sockaddr*)&client_address, &client_length);
+    if (connect_fd < 0) {
+        printf("Server accept failed...\n");
+        return -1;
+    }
+    else
+        printf("Server accepted the client...\n");
+
+    while (1) {
+
+        memset(&buffer, 0, sizeof(buffer));
+        if (read(connect_fd, buffer, sizeof(buffer)) < 0) {
+            printf("Error occured while reading from client, exiting...\n");
+            return -1;
+        }
+        if (strcmp(buffer, "shutdown") == 0) {
+            printf("Caught exit signal\n");
+            close(connect_fd);
+            close(socket_fd);
+            return 0;
+        }
+        else {
+            printf("Searching for %s\n", buffer);
+            //int sourse_fd = open(buffer, O_RDONLY);
+            if (access(buffer, F_OK) == 0) {
+                printf("File found\n");
+                sleep(1);
+                
+                printf("Sending hit signal\n");
+                while (write(connect_fd, "hit", sizeof("hit")) < 0) { // Send hit
+                    printf("Something went wrong while sending hit response, press any key to try again\n");
+                    getchar();
+                }
+            }
+
+            else {
+
+                printf("%s not found, initializing transfer from client\n", buffer);
+
+                sleep(1);
+                printf("Sending miss signal\n");
+
+                while (write(connect_fd, "miss", sizeof("miss")) < 0) { // Send miss
+                    printf("Something went wrong while sending miss response, press any key to try again\n");
+                    getchar();
+                }
+
+                printf("Waiting response\n");
+                char tempBuffer[MaxBuffer];
+                int file_fd = open(buffer, O_WRONLY | O_CREAT | O_EXCL, S_IRWXU & (~S_IXUSR));
+                int i = 1;
+                while (1) {
+                    memset(tempBuffer, 0, sizeof(tempBuffer));
+                    if (read(connect_fd, tempBuffer, sizeof(tempBuffer)) < 0) {
+                        printf("Error occured while reading from client, exiting...\n");
+                        return -1;
+                    }
+                    
+                    printf("Recieved %d packet with size %lu\n", i, sizeof(tempBuffer));
+                    i++;
+
+                    if (!file_fd) {
+                        perror("File error");
+                        return -1;
+                    }
+
+                    if (strcmp(tempBuffer, "LoLThatzAll") == 0)
+                        break;
+
+                    write(file_fd, tempBuffer, strlen(tempBuffer) - 1);
+                }
+                printf("Recieved %d packets(including termination packet)\n", i-1);
+                printf("Saving the file\n");
+                close(file_fd);
+                printf("File %s created\n", buffer);
+            }
+        }
+
+    }
+    
+
+}
